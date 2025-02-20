91ffa7cad73a4f70e41b90cf62d5034f4be11c61
Vishal Tiwari <160843582+vishal-tiwari-1971@users.noreply.github.com>
2025-02-20 17:38:41 +0530

    some changes in spiner

--- frontend/src/Components/Spinner.js
+++ frontend/src/Components/Spinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner = () => {
   return (
     <div className="flex items-center justify-center min-h-screen dark:bg-gray-900">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-white-900"></div>
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-white-900">please wait...</div>
     </div>
   );
 };
